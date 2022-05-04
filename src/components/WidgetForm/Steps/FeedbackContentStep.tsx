d87c5d9c36a6206b2a0a9a67e7175e918b4eb985
joao.bitencourt <joao.bitencourt@useall.com.br>
2022-05-04 12:55:33 -0300

    Adicionado funcionalidade de screenshot

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
@@ -12,6 +14,7 @@ export function FeedbackContentStep({
     onFeedbackRestartRequested 
 } :FeedbackContentStepProps) {
     const feedbackTypeInfo = feedbackTypes[feedbackType];
+    const [screenshot, setScreenshot] = useState<string | null>(null);
 
     return (
         <>
@@ -39,9 +42,14 @@ export function FeedbackContentStep({
                 />
 
                 <footer className="flex gap-2 mt-2">
+                    <ScreenshotButton 
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
+
                     <button
                         type="submit"
-                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
                     >
                         Enviar feedback
                     </button>
